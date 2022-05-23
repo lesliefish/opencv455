157a75a704076916700037422c15e6740e670947
lesliefish <yuleics@outlook.com>
2022-05-24 00:02:19 +0800

    第一个opencv demo

---
+++ OpenCV455/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+int main(int argc, char** argv) {
+	Mat src = imread("1.jpg");
+	namedWindow("test", WINDOW_NORMAL);
+	imshow("test", src);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
